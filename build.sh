35c536c225311048368805b0a6e15c62e780fde7
Benny Gaechter <benny.gaechter@cloudreach.com>
2022-05-10 10:08:03 +0200

    add testing and build script

---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+debug_dir="`pwd`/debug"
+release_dir="`pwd`/release"
+build_dir="`pwd`/build"
+project_dir="`pwd`"
+platform=$(uname)
+number_of_cores=1
+
+
+if [ $platform = "Darwin" ]; then
+    number_of_cores=$(sysctl -n hw.ncpu)
+elif [ $platform = "Linux" ]; then
+    number_of_cores=$(grep -c ^processor /proc/cpuinfo 2>/dev/null)
+fi
+
+function clean
+{
+	rm -rf ${build_dir}/*
+}
+
+function build
+{
+	configuration=$1
+	cd ${build_dir}
+	cmake -DCMAKE_BUILD_TYPE=${configuration^} ${project_dir}
+	make -j${number_of_cores}
+	ctest --output-on-failure
+}
+
+case $(echo "${@: -1}") in
+    -a|"")
+        build "debug"   && \
+        build "release"
+        ;;
+    -c)
+        clean
+        ;;
+    -d)
+        build "debug"
+        ;;
+    -r)
+        build "release"
+        ;;
+    *)
+        exit 1
+        ;;
+esac
--- src/maze_generator.cpp
+++ src/maze_generator.cpp
@@ -5,182 +5,176 @@
 #include <memory>
 
 struct Coordinate {
-	Coordinate( int x, int y) : x{x}, y{y} {}
-	Coordinate(const std::size_t x, const std::size_t y) : x{static_cast<int>(x)}, y{static_cast<int>(y)} {}
-	int x, y;
+  Coordinate(int x, int y) : x{x}, y{y} {}
+
+  Coordinate(const std::size_t x, const std::size_t y) : x{static_cast<int>(x)}, y{static_cast<int>(y)} {}
+
+  int x, y;
 };
 
 struct Cell {
-	Cell(int x, int y) : x(x), y(y), visited(false), is_wall(true){}
+  Cell(int x, int y) : x(x), y(y), visited(false), is_wall(true) {}
 
-	int x;
-	int y;
-	bool visited;
-	bool is_wall;
+  int x;
+  int y;
+  bool visited;
+  bool is_wall;
 };
 
 class Maze {
-	public:
-		Maze(int height, int width) : height(height), width(width)
-	{
-		generate_maze_prim();
+public:
+  Maze(int height, int width) : height(height), width(width) {
+	generate_maze_prim();
+  }
+
+  void generate_maze_prim() {
+	for (int i = 0; i < height; i++) {
+	  std::vector<std::shared_ptr<Cell>> row{};
+	  for (int j = 0; j < width; j++) {
+		row.push_back(std::make_shared<Cell>(Cell{j, i}));
+	  }
+	  map.push_back(row);
+	}
+	std::random_device dev;
+	std::mt19937 rng(dev());
+	std::uniform_int_distribution<int> rand_n_height(0, height - 1);
+	std::uniform_int_distribution<int> rand_n_width(0, width - 1);
+	int y = rand_n_height(dev);
+	int x = rand_n_width(dev);
+
+	std::vector<std::shared_ptr<Cell>> cell_stack{};
+
+	cell_stack.push_back(map[y][x]);
+
+	while (!cell_stack.empty()) {
+
+	  std::uniform_int_distribution<int> rand_element(0, cell_stack.size() - 1);
+	  int random_index = rand_element(dev);
+
+	  auto cell = *(cell_stack.begin() + random_index);
+	  cell_stack.erase(cell_stack.begin() + random_index);
+
+	  cell->visited = true;
+
+	  if (!divides_two_cooridors(cell)) {
+		cell->is_wall = false;
+	  }
+
+	  auto neighbours = get_neighbours(cell);
+	  for (auto const &neighbour : neighbours) {
+		cell_stack.push_back(neighbour);
+	  }
+	  print_maze(cell);
+	  std::cout << "\n";
 	}
 
-		void generate_maze_prim() {
-			for(int i = 0; i < height; i++) {
-				std::vector<Cell> row {};
-				for(int j = 0; j < width; j++) {
-					row.push_back(Cell{j,i});
-				}
-				map.push_back(row);
-			}
-			std::random_device dev;
-			std::mt19937 rng(dev());
-			std::uniform_int_distribution<int> rand_n_height(0,height-1);
-			std::uniform_int_distribution<int> rand_n_width(0,width-1);
-			int y = rand_n_height(dev);
-			int x = rand_n_width(dev);
+  }
 
-			std::vector<std::shared_ptr<Cell>> cell_stack{};
+  std::vector<std::shared_ptr<Cell>> get_neighbours(std::shared_ptr<Cell> cell) {
+	std::vector<std::shared_ptr<Cell>> neighbours{};
 
-			cell_stack.push_back(std::make_shared<Cell>(map[y][x]));
+	if (cell->y - 1 > 0) {
+	  auto c = map[cell->y - 1][cell->x];
+	  if (!c->visited) {
+		neighbours.push_back(c);
+	  }
+	}
+	if (cell->y - 1 > 0 && cell->x - 1 > 0) {
+	  auto c = map[cell->y - 1][cell->x - 1];
+	  if (!c->visited) {
+		neighbours.push_back(c);
+	  }
+	}
+	if (cell->y - 1 > 0 && cell->x + 1 < width) {
+	  auto c = map[cell->y - 1][cell->x + 1];
+	  if (!c->visited) {
+		neighbours.push_back(c);
+	  }
+	}
+	if (cell->x + 1 < width) {
+	  auto c = map[cell->y][cell->x + 1];
+	  if (!c->visited) {
+		neighbours.push_back(c);
+	  }
+	}
+	if (cell->x - 1 > 0) {
+	  auto c = map[cell->y][cell->x - 1];
+	  if (!c->visited) {
+		neighbours.push_back(c);
+	  }
+	}
+	if (cell->y + 1 < height) {
+	  auto c = map[cell->y + 1][cell->x];
+	  if (!c->visited) {
+		neighbours.push_back(c);
+	  }
+	}
+	if (cell->y + 1 < height && cell->x - 1 > 0) {
+	  auto c = map[cell->y + 1][cell->x - 1];
+	  if (!c->visited) {
+		neighbours.push_back(c);
+	  }
+	}
+	if (cell->y + 1 < height && cell->x + 1 < width) {
+	  auto c = map[cell->y + 1][cell->x + 1];
+	  if (!c->visited) {
+		neighbours.push_back(c);
+	  }
+	}
+	return neighbours;
+  }
 
-			while(!cell_stack.empty())
-			{
-				std::shared_ptr<Cell> cell = cell_stack.back();
-				std::cout << "Selected " << cell->x << "/" << cell->y << std::endl;
-				cell_stack.pop_back();
+  bool divides_two_cooridors(std::shared_ptr<Cell> cell) {
+	if (cell->x - 1 > 0 && cell->x + 1 < width) {
+	  if (!map[cell->y][cell->x - 1]->is_wall && !map[cell->y][cell->x + 1]->is_wall) {
+		return true;
+	  }
+	}
 
-				cell->visited = true;
+	if (cell->y - 1 > 0 && cell->y + 1 < height) {
+	  if (!map[cell->y - 1][cell->x]->is_wall && !map[cell->y + 1][cell->x]->is_wall) {
+		return true;
+	  }
+	}
+	return false;
+  }
 
-				if(!divides_two_cooridors(cell))
-				{
-					cell->is_wall = false;
-				}
-
-				auto neighbours = get_neighbours(cell);
-				for(auto const &neighbour : neighbours){
-					cell_stack.push_back(neighbour);
-				}
-
-				// for(auto c: cell_stack){
-				// std::cout << "Cell: " << c->x << "/" << c->y;
-				// if(c->is_wall)
-				// {
-				// std::cout << " is wall";
-				// }
-				// if(c->visited)
-				// {
-				// std::cout << " is visited \n" << std::endl;
-				// }
-				//
-				// }
-			}
-			// auto cell = cell_stack.front();
-			// std::cout << "Selected " << cell->x << "/" << cell->y << std::endl;
+  void print_maze() {
+	for (auto const &row : map) {
+	  for (auto const &cell : row) {
+		if (cell->is_wall) {
+		  std::cout << "x";
+		} else {
+		  std::cout << " ";
 		}
+	  }
+	  std::cout << "\n";
+	}
+  }
 
-		std::vector<std::shared_ptr<Cell>> get_neighbours(std::shared_ptr<Cell> cell) {
-			std::vector<std::shared_ptr<Cell>> neighbours{};
-
-			if ( cell->y-1 > 0 )
-			{
-				auto c = std::make_shared<Cell>(map[cell->y-1][cell->x]);
-				if(!c->visited){
-					neighbours.push_back(c);
-				}
-			}
-			if (cell->y -1 > 0 && cell->x-1 >0)
-			{
-				auto c = std::make_shared<Cell>(map[cell->y-1][cell->x-1]);
-				if(!c->visited){
-					neighbours.push_back(c);
-				}
-			}
-			if (cell->y -1 > 0 && cell->x+1 <width)
-			{
-				auto c = std::make_shared<Cell>(map[cell->y-1][cell->x+1]);
-				if(!c->visited){
-					neighbours.push_back(c);
-				}
-			}
-			if (cell->x+1 <width)
-			{
-				auto c = std::make_shared<Cell>(map[cell->y][cell->x+1]);
-				if(!c->visited){
-					neighbours.push_back(c);
-				}
-			}
-			if (cell->x-1 >0)
-			{
-				auto c = std::make_shared<Cell>(map[cell->y][cell->x-1]);
-				if(!c->visited){
-					neighbours.push_back(c);
-				}
-			}
-			if ( cell->y+1 < height )
-			{
-				auto c = std::make_shared<Cell>(map[cell->y+1][cell->x]);
-				if(!c->visited){
-					neighbours.push_back(c);
-				}
-			}
-			if (cell->y+1 < height && cell->x-1 >0)
-			{
-				auto c = std::make_shared<Cell>(map[cell->y+1][cell->x-1]);
-				if(!c->visited){
-					neighbours.push_back(c);
-				}
-			}
-			if (cell->y+1 < height && cell->x+1 <width)
-			{
-				auto c = std::make_shared<Cell>(map[cell->y+1][cell->x+1]);
-				if(!c->visited){
-					neighbours.push_back(c);
-				}
-				neighbours.push_back(c);
-			}
-			return neighbours;
+  void print_maze(std::shared_ptr<Cell> current_cell) {
+	for (auto const &row : map) {
+	  for (auto const &cell : row) {
+		if (cell == current_cell) {
+		  std::cout << "O";
+		} else if (cell->is_wall) {
+		  std::cout << "X";
+		} else {
+		  std::cout << " ";
 		}
+	  }
+	  std::cout << "\n";
+	}
+  }
 
-		bool divides_two_cooridors(std::shared_ptr<Cell> cell)
-		{
-			if (cell->x-1>0 && cell->x+1 <width)
-			{
-				if (!map[cell->y][cell->x-1].is_wall && !map[cell->y][cell->x+1].is_wall)
-				{
-					return true;
-				}
-			}
-
-			if (cell->y-1>0 && cell->y+1 <height)
-			{
-				if (!map[cell->y-1][cell->x].is_wall && !map[cell->y+1][cell->x].is_wall)
-				{
-					return true;
-				}
-			}
-			return false;
-		}
-
-		void print_maze() {
-			for(auto const &row: map){
-				for(auto const &cell : row){
-					if (cell.is_wall){
-						std::cout << "x";
-					}
-				}
-				std::cout << "\n";
-			}
-		}
-	private:
-		std::vector<std::vector<Cell>> map{{}};
-		int height;
-		int width;
+private:
+  std::vector<std::vector<std::shared_ptr<Cell>>> map{};
+  int height;
+  int width;
 };
 
 int main() {
-	std::cout << "Hello" << std::endl;
-	Maze m{10,10};
-	m.print_maze();
+  Maze m{30, 30};
+  m.print_maze();
 }
+
